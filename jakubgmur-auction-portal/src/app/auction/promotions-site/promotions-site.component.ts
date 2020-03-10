874870cfc3f7b910ce13869ff50ef6243fa25ed0
Jakub Gmur <jakubgmur@op.pl>
2020-03-10 15:54:52 +0100

    added pipe for date

--- jakubgmur-auction-portal/src/app/auction/promotions-site/promotions-site.component.ts
+++ jakubgmur-auction-portal/src/app/auction/promotions-site/promotions-site.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromotionsSiteComponent implements OnInit {
 
+  todaysDate = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
