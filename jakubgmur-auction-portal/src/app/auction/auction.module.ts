4b95935b7a1c557035f261732c5eb0a0d82f1c22
Jakub Gmur <jakubgmur@op.pl>
2020-03-10 10:39:20 +0100

    added app-auctions-site. AuctionsSiteComponent is exported

---
+++ jakubgmur-auction-portal/src/app/auction/auction.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuctionRoutingModule } from './auction-routing.module';
+import { AuctionsSiteComponent } from './auctions-site/auctions-site.component';
+
+
+@NgModule({
+  declarations: [AuctionsSiteComponent],
+  imports: [
+    CommonModule,
+    AuctionRoutingModule
+  ],
+  exports: [
+    AuctionsSiteComponent
+  ]
+})
+export class AuctionModule { }
---
+++ jakubgmur-auction-portal/src/app/auction/auctions-site/auctions-site.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auctions-site',
+  templateUrl: './auctions-site.component.html',
+  styles: []
+})
+export class AuctionsSiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
