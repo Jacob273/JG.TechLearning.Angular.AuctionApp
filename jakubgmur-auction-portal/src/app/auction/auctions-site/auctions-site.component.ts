c44f7278b84669a8b3309c6fc419753f1542aec5
Jakub Gmur <jakubgmur@op.pl>
2020-03-10 11:49:25 +0100

    added several auctions to the auctionsList in auction-site-component

--- jakubgmur-auction-portal/src/app/auction/auctions-site/auctions-site.component.ts
+++ jakubgmur-auction-portal/src/app/auction/auctions-site/auctions-site.component.ts
@@ -19,6 +19,24 @@ export class AuctionsSiteComponent implements OnInit {
     imgUrl: 'https://i.picsum.photos/id/233/200/200.jpg',
     description: 'To są super tory!',
     price: 2374
+  },
+  {
+    title: 'Wakacje',
+    imgUrl: 'https://i.picsum.photos/id/100/200/200.jpg',
+    description: 'Super wakacje!',
+    price: 7300
+  },
+  {
+    title: 'Koło',
+    imgUrl: 'https://i.picsum.photos/id/99/200/200.jpg',
+    description: 'Super koło do żuka!',
+    price: 788
+  },
+  {
+    title: 'Trawa',
+    imgUrl: 'https://i.picsum.photos/id/98/200/200.jpg',
+    description: 'Trawa do zasiania!',
+    price: 70
   }
 ];
 
