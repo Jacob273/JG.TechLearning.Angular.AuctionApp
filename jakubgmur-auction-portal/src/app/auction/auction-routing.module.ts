3b5d621d589cdf842bf06fdb68ed5bbe2ac83fc4
Jakub Gmur <jakubgmur@op.pl>
2020-03-10 15:37:43 +0100

    added promotions site component and included it in routing

--- jakubgmur-auction-portal/src/app/auction/auction-routing.module.ts
+++ jakubgmur-auction-portal/src/app/auction/auction-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuctionsSiteComponent } from './auctions-site/auctions-site.component';
+import { PromotionsSiteComponent } from './promotions-site/promotions-site.component';
 
 
 const routes: Routes = [
-  {path: 'auctions', component: AuctionsSiteComponent}
+  {path: 'auctions', component: AuctionsSiteComponent},
+  {path: 'promotions', component: PromotionsSiteComponent}
 ];
 
 @NgModule({
