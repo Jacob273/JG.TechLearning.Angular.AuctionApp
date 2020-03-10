a09ceed0e8b092b0c7402e1b7c34591716d6883f
Jakub Gmur <jakubgmur@op.pl>
2020-03-10 09:08:16 +0100

    added footer component with inline

---
+++ jakubgmur-auction-portal/src/app/footer/footer.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../menu-item';
+
+
+@Component({
+  selector: 'app-footer',
+  template: `
+  <nav class="navbar fixed-bottom navbar-expand navbar-light bg-light mt-5 p-3">
+    <ul class="navbar-nav">
+    <li class="nav-item active" *ngFor="let item of linkItems">
+      <a class="nav-link" [href]="item.link" >{{item.name}}</a>
+    </li>
+  </ul>
+</nav>
+  `,
+  styles: []
+})
+export class FooterComponent implements OnInit {
+
+  linkItems: MenuItem[] = [
+    {name: 'Polityka Prywatnosci', link: 'aaa'},
+    {name: 'Regulamin', link: 'bbb'},
+    {name: 'Pomoc', link: 'ccc'}
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
