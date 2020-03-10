bbab4b7c87f8132196e5109fc1a32423508a9f98
Jakub Gmur <jakubgmur@op.pl>
2020-03-10 15:21:54 +0100

    added routing to component inside auction-routing.module. added redirect for empty path

--- jakubgmur-auction-portal/src/app/auction/auction-routing.module.ts
+++ jakubgmur-auction-portal/src/app/auction/auction-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuctionsSiteComponent } from './auctions-site/auctions-site.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auctions', component: AuctionsSiteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
