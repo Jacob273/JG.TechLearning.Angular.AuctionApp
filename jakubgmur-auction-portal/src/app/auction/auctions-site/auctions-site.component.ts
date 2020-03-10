7e5bbe542c05a10a8cd6344f8a7c3f1478df8516
Jakub Gmur <jakubgmur@op.pl>
2020-03-10 12:02:11 +0100

    moved logic of auctions initialization from auction-site-component to auction.service

--- jakubgmur-auction-portal/src/app/auction/auctions-site/auctions-site.component.ts
+++ jakubgmur-auction-portal/src/app/auction/auctions-site/auctions-site.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuctionItem } from '../auction-item';
+import { AuctionsService } from '../auctions.service';
 
 @Component({
   selector: 'app-auctions-site',
@@ -8,41 +9,12 @@ import { AuctionItem } from '../auction-item';
 })
 export class AuctionsSiteComponent implements OnInit {
 
-    auctions: AuctionItem[] = [  {
-      title: 'Części do aparatu',
-      imgUrl: 'https://i.picsum.photos/id/36/200/200.jpg',
-      description: 'To są super aparaty.',
-      price: 2000
-  },
-  {
-    title: 'Tory',
-    imgUrl: 'https://i.picsum.photos/id/233/200/200.jpg',
-    description: 'To są super tory!',
-    price: 2374
-  },
-  {
-    title: 'Wakacje',
-    imgUrl: 'https://i.picsum.photos/id/100/200/200.jpg',
-    description: 'Super wakacje!',
-    price: 7300
-  },
-  {
-    title: 'Koło',
-    imgUrl: 'https://i.picsum.photos/id/99/200/200.jpg',
-    description: 'Super koło do żuka!',
-    price: 788
-  },
-  {
-    title: 'Trawa',
-    imgUrl: 'https://i.picsum.photos/id/98/200/200.jpg',
-    description: 'Trawa do zasiania!',
-    price: 70
-  }
-];
+    auctions: AuctionItem[];
 
-  constructor() { }
+constructor(private auctionService: AuctionsService) { }
 
   ngOnInit(): void {
+    this.auctions = this.auctionService.getAll();
   }
 
 }
