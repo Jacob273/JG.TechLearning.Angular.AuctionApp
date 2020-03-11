04012878733a22a1da29aa94091307aaa8aa4da1
Jakub Gmur <jakubgmur@op.pl>
2020-03-11 13:38:53 +0100

    Added CZ filter

--- jakubgmur-auction-portal/src/app/auction/filter-auction.pipe.ts
+++ jakubgmur-auction-portal/src/app/auction/filter-auction.pipe.ts
@@ -4,10 +4,15 @@ import { AuctionItem } from './auction-item';
 @Pipe({
   name: 'filterAuction'
 })
+// Filtr do tablicy aukcji
 export class FilterAuctionPipe implements PipeTransform {
 
-  transform(value: AuctionItem[], filterText = ''): AuctionItem[] {
-    return [];
+  transform(auctions: AuctionItem[], filterText = ''): AuctionItem[] {
+    const lowerFilterText = filterText.toLowerCase();
+
+    return auctions.filter((auction: AuctionItem) => {
+      return auction.title.toLowerCase().includes(lowerFilterText);
+    });
   }
 
 }
